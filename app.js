6255d175a277a51e12ceaba75098c3a1220a091d
kzeng24 <kathyzeng2002@gmail.com>
2023-06-20 14:35:49 -0400

    trying to make session on render work

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ mongoose.connect(CONNECTION_STRING);
 
 // configures HTTP server listening for incoming requests
 const app = express();
-app.use(express.json()); // for parsing application/json (helpful when we use ...req.body from json)
 
+app.set("trust proxy", 1);
 app.use(
   // configure cors as middleware
   // restrict cross origin resource sharing to the react application
   cors({
     credentials: true,
+    // origin: "https://a6--teal-macaron-785a52.netlify.app",
     origin: "http://localhost:3000",
   }),
   // configure server session
@@ -32,9 +33,23 @@ app.use(
     secret: "any string",
     resave: false,
     saveUninitialized: true,
-    // store: new session.MemoryStore(),
+    secure: true,
+    proxy: true,
   })
+
+  //   session({
+  //   secret: "any string",
+  //   resave: false,
+  //   saveUninitialized: false,
+  //   proxy: true,
+  //   cookie: {
+  //     sameSite: "none",
+  //     secure: true
+  //   }
+  // })
 );
+
+app.use(express.json()); // for parsing application/json (helpful when we use ...req.body from json)
 
 HelloController(app);
 UserController(app);
